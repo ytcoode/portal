791d8c04aa2e4accef70257f140f0b059046cd7f
wangyuntao <wyt.daily@gmail.com>
2020-03-03 14:40:17 +0800

    Automatically reconnect when disconnected

--- src/client.rs
+++ src/client.rs
@@ -1,28 +1,42 @@
 use futures::future;
-use futures::future::FutureExt;
+use futures::future::TryFutureExt;
+use std::time::Duration;
 use tokio::runtime::Runtime;
+use tokio::time::delay_for;
 
 mod tunnel;
 
-pub fn start<T: ToString, I: IntoIterator<Item = T>>(
+pub fn start<I, T>(
     host: &str,
     port: u16,
-    dests: I,
+    dests: T,
     debug: bool,
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> Result<(), Box<dyn std::error::Error>>
+where
+    I: ToString,
+    T: IntoIterator<Item = I>,
+{
     Runtime::new()?.block_on(async move {
-        let a = dests.into_iter().map(|dest| {
-            tokio::spawn(
-                tunnel::open(host.to_string(), port, dest.to_string()).map(move |r| {
-                    if debug {
-                        if let Err(e) = r {
-                            println!("client tunnel: {}", e);
-                        }
-                    }
-                }),
-            )
+        dests.into_iter().for_each(|dest| {
+            open_tunnel(host.to_string(), port, dest.to_string(), debug);
         });
-        future::join_all(a).await;
-        Ok(())
+        future::pending().await
     })
+}
+
+fn open_tunnel(host: String, port: u16, dest: String, debug: bool) {
+    tokio::spawn(
+        tunnel::open(host.clone(), port, dest.clone())
+            .inspect_err(move |e| {
+                if debug {
+                    println!("client tunnel: {}", e);
+                }
+            })
+            .or_else(move |_| async move {
+                delay_for(Duration::from_secs(1)).await;
+                println!("Reconnecting: {}:{}", host, port);
+                open_tunnel(host, port, dest, debug);
+                Ok::<(), ()>(())
+            }),
+    );
 }
